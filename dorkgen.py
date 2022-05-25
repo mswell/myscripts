e223dff402e080432647716de87bc36e96997e08
Wellington Moraes <wellpunk@gmail.com>
2022-05-25 16:38:46 -0300

    added dorks to login page

--- dorkgen.py
+++ dorkgen.py
@@ -20,9 +20,13 @@ open_redirect = [
 ]
 
 
+print('Dorks for Open Redirect')
 for dork in open_redirect:
     google_url = f"https://www.google.com/search?q=site%3A{subdomain}+inurl%3A{dork}"
     print(google_url)
-    
+
+print("\nDorks to login page\n")
+login_page = f"https://www.google.com/search?q=site:{subdomain}+inurl:login+%7C+inurl:signin+%7C+intitle:Login+%7C+intitle:%22sign+in%22+%7C+inurl:auth"
+print(login_page)
     
     
